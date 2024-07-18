388073fb9de452ae44c898ab483bc47a474917c3
Onstage2426 <machiel-@tuta.io>
2024-07-19 01:38:20 +0200

    added transparancy

--- .config/nvim/lua/plugins/rose-pine.lua
+++ .config/nvim/lua/plugins/rose-pine.lua
@@ -1,7 +1,16 @@
 return {
-    "rose-pine/neovim", 
+    "rose-pine/neovim",
     name = "rose-pine",
     config = function()
-        vim.cmd('colorscheme rose-pine')
+        require("rose-pine").setup({
+            styles = {
+                transparency = true,
+            },
+        })
+
+        vim.cmd("colorscheme rose-pine")
+        -- vim.cmd("colorscheme rose-pine-main")
+        -- vim.cmd("colorscheme rose-pine-moon")
+        -- vim.cmd("colorscheme rose-pine-dawn")
     end
 }
